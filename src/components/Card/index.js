30442a93c758ed460fb09fac0dac71ac082f050a
Lucas Smaniotto <lucas91755@gmail.com>
2023-01-04 14:03:28 -0300

    feat: adicionando icones react

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,14 @@
 import './index.css';
+import { CgCloseR } from 'react-icons/cg';
 
 const Card = ({name, image, charge, color}) => {
+  
+  const deleteCard = () => {
+    console.log('deletar');
+  };
+  
   return (<div className="card__wrapper">
+    <CgCloseR className="delete__button" onClick={deleteCard} />
     <div className="card__header" style={{ backgroundColor: color}}>
         <img src={image} alt={name} className='card__img'/>
     </div>
@@ -9,8 +16,7 @@ const Card = ({name, image, charge, color}) => {
         <h4 className='card__name'>{name}</h4>
         <h3 className='card__charge'>{charge}</h3>
     </div>
-  </div>
-  )
+  </div>)
 }
 
 export default Card;
